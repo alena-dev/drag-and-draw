bfe97931501bc3a0a6a66ab7715c22675addec47
alena <49859442+alena-dev@users.noreply.github.com>
2019-01-22 22:05:50 +0300

    Task-1. Paint inside onDraw(Canvas)

--- app/src/main/java/com/g/e/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/g/e/draganddraw/BoxDrawingView.java
@@ -1,6 +1,8 @@
 package com.g.e.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.support.v4.content.FileProvider;
 import android.util.AttributeSet;
@@ -16,6 +18,8 @@ public class BoxDrawingView extends View {
 
     private Box mCurrentBox;
     private List<Box> mBoxen = new ArrayList<>();
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
 
     public BoxDrawingView(Context context) {
         this (context, null);
@@ -23,6 +27,25 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView (Context context, AttributeSet attrs){
         super(context, attrs);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroundPaint);
+
+        for(Box box : mBoxen){
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
     }
 
     @Override
