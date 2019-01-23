94019ae35e983bb8e07265d9a066b6b88caaa571
alena <49859442+alena-dev@users.noreply.github.com>
2019-01-23 12:15:53 +0300

    Task-1-1. Save instance for view

--- app/src/main/java/com/g/e/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/g/e/draganddraw/BoxDrawingView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -15,11 +18,13 @@ import java.util.List;
 
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+    private String SUPER_INSTANCE_STATE = "super_instance_state";
 
     private Box mCurrentBox;
     private List<Box> mBoxen = new ArrayList<>();
     private Paint mBoxPaint;
     private Paint mBackgroundPaint;
+
 
     public BoxDrawingView(Context context) {
         this (context, null);
@@ -46,6 +51,34 @@ public class BoxDrawingView extends View {
             float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
             canvas.drawRect(left, top, right, bottom, mBoxPaint);
         }
+    }
+
+    @Nullable
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(SUPER_INSTANCE_STATE, super.onSaveInstanceState());
+        ArrayList<Parcelable> boxen = new ArrayList<Parcelable>();
+
+        for(Box box : mBoxen){
+            boxen.add(box);
+        }
+
+        bundle.putParcelableArrayList(TAG, boxen);
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        Bundle bundle = (Bundle)state;
+
+        ArrayList<Parcelable> boxen = bundle.getParcelableArrayList(TAG);
+        for (Parcelable box : boxen){
+            mBoxen.add((Box)box);
+        }
+        invalidate();
+
+        super.onRestoreInstanceState(bundle.getParcelable(SUPER_INSTANCE_STATE));
     }
 
     @Override
