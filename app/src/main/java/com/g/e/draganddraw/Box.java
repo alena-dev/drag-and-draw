6aef5cefaffea6002874c7eab24423668de43014
alena <49859442+alena-dev@users.noreply.github.com>
2019-01-24 19:59:56 +0300

    Task-1-2. Rotate rect by second finger

--- app/src/main/java/com/g/e/draganddraw/Box.java
+++ app/src/main/java/com/g/e/draganddraw/Box.java
@@ -3,12 +3,22 @@ package com.g.e.draganddraw;
 import android.graphics.PointF;
 
 public class Box {
-    private PointF mOrigin;
-    private PointF mCurrent;
+    private PointF mOrigin = new PointF();
+    private PointF mCurrent = new PointF();
 
-    public Box (PointF origin){
-        mOrigin = origin;
-        mCurrent = origin;
+
+    private float mDegrees;
+
+//    public Box (PointF origin){
+//        mOrigin = origin;
+//        mCurrent = origin;
+//        mDegrees = 0;
+//    }
+
+    public Box (float originX, float originY) {
+        mOrigin.set(originX, originY);
+        mCurrent.set(originX, originY);
+        mDegrees = 0;
     }
 
     public PointF getCurrent() {
@@ -19,7 +29,30 @@ public class Box {
         mCurrent = current;
     }
 
+    public void setCurrent (float x, float y){
+        mCurrent.set(x, y);
+    }
+
     public PointF getOrigin() {
         return mOrigin;
+    }
+
+    public float getDegrees() {
+        return mDegrees;
+    }
+
+    public void setDegrees(PointF startPosition, PointF endPosition) {
+        double side1 = Math.sqrt(Math.pow((mCurrent.x - startPosition.x), 2)
+        + Math.pow((mCurrent.y - startPosition.y), 2));
+        double side2 =  Math.sqrt(Math.pow((mCurrent.x - endPosition.x), 2)
+                + Math.pow((mCurrent.y - endPosition.y), 2));
+        double side3 = Math.sqrt(Math.pow((startPosition.x - endPosition.x), 2)
+                + Math.pow((startPosition.y - endPosition.y), 2));
+
+        double arccos = Math.acos((Math.pow(side1, 2) + Math.pow(side2, 2) - Math.pow(side3, 2))/
+                (2*side1*side2));
+
+        float degrees = (float)(arccos*180/Math.PI);
+        mDegrees = degrees;
     }
 }
--- app/src/main/java/com/g/e/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/g/e/draganddraw/BoxDrawingView.java
@@ -2,8 +2,11 @@ package com.g.e.draganddraw;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.v4.content.FileProvider;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -20,6 +23,12 @@ public class BoxDrawingView extends View {
     private List<Box> mBoxen = new ArrayList<>();
     private Paint mBoxPaint;
     private Paint mBackgroundPaint;
+
+    private int mPaintFingerId = -1;
+    private int mRotationFingerId = -1;
+    private PointF mOriginPoint;
+    private PointF mCurrentPoint;
+
 
     public BoxDrawingView(Context context) {
         this (context, null);
@@ -44,40 +53,122 @@ public class BoxDrawingView extends View {
             float right = Math.max(box.getOrigin().x, box.getCurrent().x);
             float top = Math.min(box.getOrigin().y, box.getCurrent().y);
             float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+//            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+
+            canvas.save();
+            canvas.rotate(box.getDegrees(), box.getCurrent().x, box.getCurrent().y);
             canvas.drawRect(left, top, right, bottom, mBoxPaint);
+            canvas.restore();
+
+//            RectF r = new RectF();
+//            r.set(left, top, right, bottom);
+//            Matrix m = new Matrix();
+//            m.setRotate(45, box.getOrigin().x, box.getCurrent().x);
+//            m.mapRect(r);
+
         }
+
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        PointF current = new PointF(event.getX(), event.getY());
-        String action = "";
 
+        String maskedAction = "";
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_POINTER_DOWN:
+                maskedAction = "ACTION_POINTER_DOWN";
+
+                if(mPaintFingerId !=-1 && mRotationFingerId == -1){
+                    mRotationFingerId = event.getPointerId(event.getActionIndex());
+                    mCurrentPoint = new PointF();
+                    mCurrentPoint.set(event.getX(event.getActionIndex()), event.getY(event.getActionIndex()));
+                    if (mOriginPoint==null) {
+                        mOriginPoint = new PointF();
+                        mOriginPoint.set(event.getX(event.getActionIndex()), event.getY(event.getActionIndex()));
+                    }
+                }
+
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                maskedAction = "ACTION_POINTER_UP";
+
+                if (event.findPointerIndex(mRotationFingerId) == event.getActionIndex()){
+                    mRotationFingerId = -1;
+                    mCurrentPoint = null;
+                }
+
+                if (event.findPointerIndex(mPaintFingerId) == event.getActionIndex()){
+                    mPaintFingerId = -1;
+                    mRotationFingerId = -1;
+                    mCurrentPoint = null;
+                    mOriginPoint = null;
+                    mCurrentBox = null;
+                }
+
+                break;
+        }
+
+        String action = "";
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
-                mCurrentBox = new Box(current);
+
+                mPaintFingerId = event.getPointerId(0);
+
+                mCurrentBox = new Box(event.getX(0), event.getY(0));
                 mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
-                if (mCurrentBox != null){
-                    mCurrentBox.setCurrent(current);
+                if (mCurrentBox != null) {
+                    int paintFingerIndex = event.findPointerIndex(mPaintFingerId);
+                    mCurrentBox.setCurrent(event.getX(paintFingerIndex), event.getY(paintFingerIndex));
+                    if(mRotationFingerId>=0) {
+                        int rotationFingerIndex = event.findPointerIndex(mRotationFingerId);
+                        mCurrentPoint.set(event.getX(rotationFingerIndex), event.getY(rotationFingerIndex));
+                        mCurrentBox.setDegrees(mOriginPoint, mCurrentPoint);
+                    }
+
                     invalidate();
                 }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+
+                mPaintFingerId = -1;
+                mRotationFingerId = -1;
+
                 mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+
+                mPaintFingerId = -1;
+                mRotationFingerId = -1;
+
                 mCurrentBox = null;
                 break;
+
         }
 
-        Log.i(TAG, action + "at x=" + current.x +
-                ", y=" + current.y);
+//        Log.i(TAG, action + " at x=" + current.x +
+//                ", y=" + current.y);
+        if (maskedAction!="") {
+            Log.i(TAG, maskedAction + " " + event.getPointerCount());
+        }
+        if(action!=""){
+            Log.i(TAG, action + " " + event.getPointerCount());
+        }
+        for (int i=0; i<event.getPointerCount(); i++){
+            Log.i(TAG, "index-" + i + " ID-" + event.getPointerId(i) + " x-" + event.getX(i) + " y-" + event.getY(i));
+        }
+
+//        Log.i(TAG, "first - (x,y)");
+//        if (isSecondFingerPressed){
+//            Log.i(TAG, "second - (x,y)");
+//        }
+
 
         return true;
     }
